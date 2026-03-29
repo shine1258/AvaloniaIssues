8f34167244ad9210cb7118e91f1194558231eb66
shine1258 <shine2048@outlook.com>
2026-03-30 01:03:27 +0800

    Init and add GhostItemView.

---
+++ Views/GhostItemView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Avalonia.Controls;
+
+namespace AvaloniaIssues.Views;
+
+public partial class GhostItemView : UserControl
+{
+    public GhostItemView() => InitializeComponent();
+
+    public Item[] Items { get; } =
+        Enumerable.Range(1, 20).Select(i => new Item(i, i % 2 == 1)).ToArray();
+
+    public record Item(int Index, bool ShowMore);
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace AvaloniaIssues.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+}
